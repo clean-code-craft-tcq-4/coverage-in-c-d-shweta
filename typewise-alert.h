2b284906ea7113dda2d3420348e7c3c65e984bdc
d-shweta <105386129+d-shweta@users.noreply.github.com>
2022-07-28 02:31:43 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -3,7 +3,8 @@
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
+  MED_ACTIVE_COOLING,
+  MAX_COOLINGTYPE
 } CoolingType;
 
 typedef enum {
@@ -12,21 +13,33 @@ typedef enum {
   TOO_HIGH
 } BreachType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
-
-typedef enum {
-  TO_CONTROLLER,
-  TO_EMAIL
-} AlertTarget;
+typedef struct {
+    double lowerLimit;
+    double higherLimit;
+} Limits;
 
 typedef struct {
   CoolingType coolingType;
   char brand[48];
 } BatteryCharacter;
 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
+#define PASSIVE_COOLING_LOWLIMIT     0
+#define PASSIVE_COOLING_HIGHLIMIT    35
+#define MED_ACTIVE_COOLING_LOWLIMIT  PASSIVE_COOLING_LOWLIMIT
+#define MED_ACTIVE_COOLING_HIGHLIMIT 40
+#define HI_ACTIVE_COOLING_LOWLIMIT   PASSIVE_COOLING_LOWLIMIT
+#define HI_ACTIVE_COOLING_HIGHLIMIT  45
+
+#define MSG_HEADER 0xfeed
+
+#define RECEPIENT        "a.b@c.com"
+#define MSG_TOO_LOW      "Hi, the temperature is too low\n"
+#define MSG_TOO_HIGH     "Hi, the temperature is too high\n"
+#define MSG(message) 	  MSG_##message 
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+void checkAndAlert(BatteryCharacter batteryChar, double temperatureInC, void (*alertType) (BreachType breachType));
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
